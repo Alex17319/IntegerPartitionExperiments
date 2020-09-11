c9f34d29812f0a70ad28e57338a921de4262fd8e
Alex Priest <alex@westminstersoftware.com>
2020-09-11 22:22:59 +0930

    Begin extended version of v9, where the completed expansion register would be used as a quick lookup in order to continue with a normal tree search (with the tree cut short by using the register)

---
+++ C++ v9-extended/math-utils.cpp
@@ -0,0 +1,76 @@
+#include "math-utils.h"
+#include <cmath>
+#include <iostream>
+#include <stdint.h>
+
+using namespace std;
+
+int math_power_copy;
+uint64_t math_n_copy;
+
+uint64_t threePowers[40] = {
+	1ull, 3ull, 9ull, 27ull, 81ull, 243ull, 729ull, 2187ull, 6561ull, 19683ull, 59049ull, 177147ull,
+	531441ull, 1594323ull, 4782969ull, 14348907ull, 43046721ull, 129140163ull, 387420489ull, 1162261467ull,
+	3486784401ull, 10460353203ull, 31381059609ull, 94143178827ull, 282429536481ull, 847288609443ull,
+	2541865828329ull, 7625597484987ull, 22876792454961ull, 68630377364883ull, 205891132094649ull,
+	617673396283947ull, 1853020188851841ull, 5559060566555523ull, 16677181699666569ull, 50031545098999707ull,
+	150094635296999121ull, 450283905890997363ull, 1350851717672992089ull, 4052555153018976267ull
+};
+//	uint64_t threeToThe(int power) {
+//		if (power >= 40) {
+//			cout << "Error: overflow in 3^n function" << endl;
+//			exit(-1);
+//		}
+//		return threePowers[power];
+//	}
+
+//Based on https://stackoverflow.com/a/23000588/4149474 which is based on https://stackoverflow.com/a/11398748/4149474
+//which is based on https://graphics.stanford.edu/~seander/bithacks.html#IntegerLogDeBruijn
+//"It's correct for all inputs except 0. It returns 0 for 0 which may be valid for what you're using it for. The lines
+//with the shifts round n up to 1 less than the next power of 2. It basically sets all bits after the leading 1 bit to 1.
+//This reduces all possible inputs to 64 possible values: 0x0, 0x1, 0x3, 0x7, 0xf, 0x1f, 0x3f, etc. Multiplying those 64
+//values with the number 0x03f6eaf2cd271461 gives you another 64 unique values in the top 6 bits. The shift by 58 just
+//positions those 6 bits for use as an index into table."
+//Also, "0x03f6eaf2cd271461 is a De Bruijn sequence" (nwellnhof 2017)
+char floorLog2Lookup_64bit[64] = {
+	0, 58, 1, 59, 47, 53, 2, 60, 39, 48, 27, 54, 33, 42, 3, 61,
+	51, 37, 40, 49, 18, 28, 20, 55, 30, 34, 11, 43, 14, 22, 4, 62,
+	57, 46, 52, 38, 26, 32, 41, 50, 36, 17, 19, 29, 10, 13, 21, 56,
+	45, 25, 31, 35, 16, 9, 12, 44, 24, 15, 8, 23, 7, 6, 5, 63
+};
+//	char floorLog2_64bit(uint64_t n)
+//	{
+//		n |= n >> 1;
+//		n |= n >> 2;
+//		n |= n >> 4;
+//		n |= n >> 8;
+//		n |= n >> 16;
+//		n |= n >> 32;
+//	
+//		return floorLog2Lookup_64bit[(n * 0x03f6eaf2cd271461) >> 58];
+//	}
+
+// Takes the bits of x, and OR's the lower half into the even numbered positions (zero indexed) of *low,
+// and the upper half into the even numbered positions of *high
+// Adapted from: http://www.graphics.stanford.edu/~seander/bithacks.html#InterleaveBMN
+void spreadAndOrBits(uint64_t x, uint64_t *low, uint64_t *high) {
+	uint64_t xLow = x & 0x00000000FFFFFFFF;
+	uint64_t xHigh = (x & 0xFFFFFFFF00000000) >> 32;
+	
+	xLow = (xLow | (xLow << 16)) & 0x0000FFFF0000FFFF; //16 0's, 16 1's, 16 0's, 16 1's
+	xLow = (xLow | (xLow << 8 )) & 0x00FF00FF00FF00FF; //8 0's, 8 1's, 8 0's, ...
+	xLow = (xLow | (xLow << 4 )) & 0x0F0F0F0F0F0F0F0F; //00001111...
+	xLow = (xLow | (xLow << 2 )) & 0x3333333333333333; //00110011...
+	xLow = (xLow | (xLow << 1 )) & 0x5555555555555555; //0101...
+	
+	xHigh = (xHigh | (xHigh << 16)) & 0x0000FFFF0000FFFF;
+	xHigh = (xHigh | (xHigh << 8 )) & 0x00FF00FF00FF00FF;
+	xHigh = (xHigh | (xHigh << 4 )) & 0x0F0F0F0F0F0F0F0F;
+	xHigh = (xHigh | (xHigh << 2 )) & 0x3333333333333333;
+	xHigh = (xHigh | (xHigh << 1 )) & 0x5555555555555555;
+	
+	*low |= xLow;
+	*high |= xHigh;
+}
+
+void 
---
+++ C++ v9-extended/two-three-decisions.cpp
@@ -0,0 +1,515 @@
+#include "math-utils.h"
+#include <atomic>
+#include <bitset>
+#include <chrono>
+#include <climits>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+#include <stdint.h>
+
+using namespace std;
+
+const int CHUNK_BITS = 64;
+
+struct expansionRegisterColumn {
+	uint64_t *array;
+	uint64_t capacity; // measured in CHUNK_BITS-long chunks (i.e. the number of uint64_t's in the array)
+	uint64_t currentArrayPos; // measured in bits
+	uint64_t currentVal;
+	uint64_t size; // measured in CHUNK_BITS-long chunks
+};
+
+// prints from bytes in memory, so affected by endianness
+void printIntBits_memory(unsigned int x) {
+	char bytes[sizeof(unsigned int)];
+	*((unsigned int *)(&(bytes[0]))) = x;
+	for (int i = 0; i < sizeof(bytes); i++) {
+		if (i > 0) cout << ":";
+		cout << bitset<8>(bytes[i]).to_string('-', '#');
+	}
+}
+
+// prints from bytes in memory, so affected by endianness
+void printUInt64Bits_memory(uint64_t x) {
+	char bytes[sizeof(uint64_t)];
+	*((uint64_t *)(&(bytes[0]))) = x;
+	for (int i = 0; i < sizeof(bytes); i++) {
+		if (i > 0) cout << ":";
+		cout << bitset<8>(bytes[i]).to_string('-', '#');
+	}
+}
+
+// prints the number the way mathematical operations see it
+void printUInt64Bits_cpu(uint64_t x) {
+	string b = bitset<64>(x).to_string('-', '#');
+	for (int i = 0; i < 8; i++) {
+		if (i > 0) cout << ":";
+		cout << b.substr(i * 8, 8);
+	}
+}
+
+// prints the number the way mathematical operations see it, but reversed
+void printUInt64Bits_cpu_reverse(uint64_t x) {
+	string b = bitset<64>(x).to_string('-', '#');
+	for (int i = 63; i >= 0; i--) {
+		cout << b[i];
+		//if (i > 0 && i % 8 == 0) cout << ":";
+	}
+}
+
+// can give false negatives, will not give false positives
+bool hasONbitsAtMultiplesOf3(uint64_t x) {
+	uint64_t mask = 0x9249249249249249; // 1001001...1001001
+	return ((x & mask) != 0) && ((x & (mask << 1)) != 0) && ((x & (mask << 2)) != 0);
+	//	return ((~x & mask) != mask) && ((~x & (mask << 1)) != (mask << 1)) && ((~x & (mask << 2)) != (mask << 2));
+}
+
+struct expansionRegisterColumn createExpansionRegCol(
+	uint64_t capacity,
+	uint64_t currentArrayPos,
+	uint64_t currentVal,
+	uint64_t firstChunkContents
+) {
+	struct expansionRegisterColumn result;
+	result.array = new uint64_t[capacity]();
+	
+	result.capacity = capacity;
+	result.currentArrayPos = currentArrayPos;
+	result.currentVal = currentVal;
+	
+	result.array[0] = firstChunkContents;
+	
+	result.size = 1;
+	
+	return result;
+}
+
+void stepForwardChunk(struct expansionRegisterColumn *expRegCol) {
+	
+	expRegCol->size--;
+	
+	// clear whole previous chunk (including bits that were already behind us),
+	// and don't clear skipped bits of the newly entered chunk
+	expRegCol->array[expRegCol->currentArrayPos / CHUNK_BITS] = 0;
+	
+	uint64_t step = CHUNK_BITS;
+	expRegCol->currentVal += step;
+	expRegCol->currentArrayPos = (expRegCol->currentArrayPos + step) % (expRegCol->capacity * CHUNK_BITS);
+}
+
+bool valueInWriteableRange(struct expansionRegisterColumn *expRegCol, uint64_t value) {
+	// check if value is not stored/storable any more as its too small
+	if (value < expRegCol->currentVal) return false;
+	
+	uint64_t distToVal = value - expRegCol->currentVal;
+	
+	// bits we've gone past, but that are in the chunk we're still in,
+	// so can't wrap around and write in them again yet
+	uint64_t unusableBits = expRegCol->currentArrayPos % CHUNK_BITS;
+	
+	return distToVal < (expRegCol->capacity * CHUNK_BITS - unusableBits);
+}
+
+void expand(struct expansionRegisterColumn *expRegCol) {
+	struct expansionRegisterColumn result;
+	result.array = new uint64_t[expRegCol->capacity * 2]();
+	
+	result.capacity = expRegCol->capacity * 2;
+	result.currentArrayPos = expRegCol->currentArrayPos % CHUNK_BITS;
+	result.currentVal = expRegCol->currentVal;
+	result.size = expRegCol->size;
+	
+	uint64_t copyStartPos = expRegCol->currentArrayPos / CHUNK_BITS;
+	for (uint64_t i = 0; i < expRegCol->size; i++) {
+		result.array[i] = expRegCol->array[(copyStartPos + i) % expRegCol->capacity];
+	}
+	
+	delete[] expRegCol->array;
+	*expRegCol = result;
+}
+
+uint64_t *getChunkHolding(
+	struct expansionRegisterColumn *expRegCol,
+	uint64_t value,
+	int *offsetIntoChunk,
+	uint64_t *minSizeIfWriteHere
+) {
+	if (value < expRegCol->currentVal) {
+		throw out_of_range(
+			string("Error: flag at value '")
+			+ to_string(value)
+			+ "' not stored any more in expansionRegisterColumn, as value is smaller than currentVal '"
+			+ to_string(expRegCol->currentVal)
+			+ "'."
+		);
+	}
+	
+	while (!valueInWriteableRange(expRegCol, value)) {
+		expand(expRegCol);
+	}
+	
+	uint64_t distToVal = value - expRegCol->currentVal;
+	uint64_t valPos = expRegCol->currentArrayPos + distToVal;
+	
+	// we've already ensured that value is in the writeable range, so its safe to just wrap valPos around.
+	uint64_t *valChunk = expRegCol->array + ((valPos / CHUNK_BITS) % expRegCol->capacity);
+	
+	*offsetIntoChunk = valPos % CHUNK_BITS;
+	*minSizeIfWriteHere = valPos/CHUNK_BITS - expRegCol->currentArrayPos/CHUNK_BITS + 1;
+	
+	return valChunk;
+}
+
+// the process is more complex for first chunk (as doubling
+// can leave you in the same chunk; in later chunks it never does)
+void initialiseExpRegFirstChunk(vector<struct expansionRegisterColumn> *expReg, uint64_t startSize) {
+	expReg->push_back(createExpansionRegCol(startSize, 0, 0, 1ULL << 1));
+	for (int i = 0; threeToThe(i) <= CHUNK_BITS; i++) {
+		// for each ON bit at some position j, turn the bit at 2*j ON
+		for (int j = 1; j * 2 < CHUNK_BITS; j++) {
+			// must not run this for j * 2 = 64 or above, otherwise we'll be bitshifting by >=64 bits,
+			// which is undefined behaviour (and on my machine seems to wrap around sometimes)
+			
+			if (((*expReg)[i].array[0] & (1ULL << j)) != 0) {
+				(*expReg)[i].array[0] |= (1ULL << (j * 2));
+			}
+		}
+		
+		if (threeToThe(i) >= CHUNK_BITS) break; // don't do the next part on the last iteration
+		
+		expReg->push_back(createExpansionRegCol(startSize, 0, 0, 0));
+		
+		// in expansion-register-column i (which represents 3^i being the last added power of 3),
+		// for each ON bit at some position j, turn the bit at j + 3^(i+1) ON in expansion-register-column i+1
+		for (int j = 1; (j + threeToThe(i + 1)) < CHUNK_BITS; j++) {
+			if (((*expReg)[i].array[0] & (1ULL << j)) != 0) {
+				(*expReg)[i + 1].array[0] |= (1ULL << (j + threeToThe(i + 1)));
+			}
+		}
+	}
+}
+
+bool columnHasStarted(struct expansionRegisterColumn *expRegCol, int colNum, uint64_t curValue) {
+	if (expRegCol->currentVal / CHUNK_BITS < curValue / CHUNK_BITS) {
+		throw logic_error("Assertion failed: missed some chunks");
+	}
+	
+	return expRegCol->currentVal / CHUNK_BITS <= curValue / CHUNK_BITS;
+}
+
+uint64_t getCurrentChunkContents(struct expansionRegisterColumn *expRegCol) {
+	return expRegCol->array[expRegCol->currentArrayPos / CHUNK_BITS];
+}
+
+void growExpRegIfLastColumn(vector<struct expansionRegisterColumn> *expReg, int colNum, uint64_t pow, uint64_t startSize) {
+	struct expansionRegisterColumn *currentCol = &((*expReg)[colNum]);
+	
+	if (colNum == expReg->size() - 1) {
+		expReg->push_back(
+			createExpansionRegCol(
+				startSize, // capacity
+				0, // currentArrayPos
+				currentCol->currentVal + pow - (currentCol->currentVal + pow) % CHUNK_BITS, // currentVal
+				0 //firstChunkContents
+			)
+		);
+	}
+}
+
+void copyAlongByPowerOfThree(vector<struct expansionRegisterColumn> *expReg, int colNum, uint64_t chunkContents, uint64_t startSize) {
+	uint64_t pow = threeToThe(colNum + 1);
+	
+	growExpRegIfLastColumn(expReg, colNum, pow, startSize);
+	
+	struct expansionRegisterColumn *currentCol = &((*expReg)[colNum]);
+	struct expansionRegisterColumn *nextCol = &((*expReg)[colNum + 1]);
+	
+	uint64_t minSizeAfterWrite;
+	int offsetIntoDestChunk;
+	uint64_t *destChunkTwo = getChunkHolding(
+		nextCol,
+		currentCol->currentVal + pow + CHUNK_BITS,
+		&offsetIntoDestChunk, // will be the same for first and second dest chunks, i.e. regardless of adding any multiple of CHUNK_BITS
+		&minSizeAfterWrite
+	);
+	uint64_t offsetToTwo = destChunkTwo - nextCol->array;
+	uint64_t *destChunkOne = nextCol->array + (offsetToTwo + nextCol->capacity - 1) % nextCol->capacity;
+	
+	int offsetIntoCurrentChunk = currentCol->currentArrayPos % CHUNK_BITS;
+	
+	*destChunkOne |= ((chunkContents >> offsetIntoCurrentChunk) << offsetIntoDestChunk);
+	*destChunkTwo |= ((chunkContents >> offsetIntoCurrentChunk) >> (CHUNK_BITS - offsetIntoDestChunk));
+	
+	nextCol->size = max(nextCol->size, minSizeAfterWrite);
+}
+
+void copyAlongToDoubleCurrentPos(struct expansionRegisterColumn *expRegCol, uint64_t chunkContents) {
+	uint64_t minSizeAfterWrite;
+	int ignored;
+	uint64_t *destChunkTwo = getChunkHolding(
+		expRegCol,
+		expRegCol->currentVal * 2 + CHUNK_BITS,
+		&ignored,
+		&minSizeAfterWrite
+	);
+	uint64_t offsetToTwo = destChunkTwo - expRegCol->array;
+	uint64_t *destChunkOne = expRegCol->array + (offsetToTwo + expRegCol->capacity - 1) % expRegCol->capacity;
+	
+	spreadAndOrBits(chunkContents, destChunkOne, destChunkTwo);
+	
+	expRegCol->size = max(expRegCol->size, minSizeAfterWrite);
+}
+
+void populateColOneChunk(uint64_t curValue, uint64_t* chunkAggregate) {
+	uint64_t chunkContents = 0;
+	uint64_t chunkNum = curValue / CHUNK_BITS;
+	if (chunkNum == 0) { // full value when chunk number == 0
+		chunkContents = 0b00000001'00000000'00010001'01001001'00010000'01011001'01001101'10110000;
+	} else {
+		// when chunk number == 1
+		if (chunkNum == 1) {
+			chunkContents |= 0b00000000'00000000'00000000'00000000'00000000'00000001'00000000'00000000;
+		}
+		
+		// whenever chunk number == 2^n + 1 for some non-negative integer n
+		if (isPowerOf2_safe(chunkNum - 1)) { // isPowerOf2_safe() is fine for input zero, i.e. when chunkNum == 1
+			chunkContents |= 0b00000000'00000000'00000000'00000001'00000000'00000000'00000000'00000000;
+		}
+		
+		// whenever chunk number == 2^n for some non-negative integer n
+		if (isPowerOf2(chunkNum)) { // isPowerOf2() breaks for zero but we've already checked it's not zero
+			chunkContents |= 0b00000000'00000001'00000000'00000000'00000001'00000000'00010000'01001001;
+			return; // already set the LSB ON, so no need to check the next rule, which just does that
+		}
+		
+		// whenever chunk number == 2^n + 3*2^m where n >= m for some non-negative integers n and m
+		
+		// This condition corresponds to chunk numbers of any of the following forms:
+		//  (1) ...1...
+		//  (2) ...10...11...
+		//  (3) ...111...
+		//  (4) ...101...
+		// where ... means '0 repeated zero or more times'
+		// The first type corresponds to he power of 2, which we've already checked
+		// For other types, we clear the least significant bit & extract what it was,
+		// do the same again, and then check if either:
+		//  - The two extracted bits are adjacent to each other, and what's left over has
+		//    a single bit on (types 2 & 3).
+		//  - The two extracted bits are separated by exactly one zero, and what's left
+		//    over is just zero (type 4).
+		
+		uint64_t oneBitCleared = chunkNum & (chunkNum - 1); // clear the least significant 1 bit
+		uint64_t clearedBit1 = chunkNum & !oneBitCleared;   // extract just the bit that was cleared
+		uint64_t twoBitsCleared = oneBitCleared & (oneBitCleared - 1);
+		uint64_t clearedBit2 = oneBitCleared & !twoBitsCleared;
+		
+		// if the two least significant ON bits in chunkNum are adjacent, then adjMask
+		// will contain exactly 1 ON bit, i.e. it will be a power of 2.
+		uint64_t adjMask = clearedBit1 & (clearedBit2 >> 1);
+		
+		// Here we start with:
+		// isPowerOf2_safe(adjMask) && isPowerOf2_safe(twoBitsCleared) || twoBitsCleared == 0 && isPowerOf2_safe(oneApartMask)
+		// Where oneApartMask = clearedBit1 & (clearedBit2 >> 2), so the same thing as adjMask but checking
+		// if they're separated by a single OFF bit
+		// 
+		// Then simplify:
+		//
+		// Note: isPowerOf2_safe(x) is implemented as: x && !(x & (x - 1))
+		// The part in brackets corresponds to "x, with the last ON bit cleared" (relevant for some of the simplification)
+		// 
+		// isPowerOf2_safe(adjMask) && isPowerOf2_safe(twoBitsCleared) || twoBitsCleared == 0 && isPowerOf2_safe(oneApartMask)
+		// (adjMask && !(adjMask & (adjMask - 1))) && (twoBitsCleared && !(twoBitsCleared & (twoBitsCleared - 1))) || twoBitsCleared == 0 && (oneApartMask && !(oneApartMask & (oneApartMask - 1)))
+		// adjMask && !(adjMask & (adjMask - 1)) && twoBitsCleared && !(twoBitsCleared & (twoBitsCleared - 1)) || !twoBitsCleared && oneApartMask && !(oneApartMask & (oneApartMask - 1))
+		// twoBitsCleared && adjMask && !(adjMask & (adjMask - 1)) && !(twoBitsCleared & (twoBitsCleared - 1)) || !twoBitsCleared && oneApartMask && !(oneApartMask & (oneApartMask - 1))
+		// twoBitsCleared && adjMask && !(adjMask & (adjMask - 1) | twoBitsCleared & (twoBitsCleared - 1)) || !twoBitsCleared && oneApartMask && !(oneApartMask & (oneApartMask - 1))
+		// twoBitsCleared && adjMask && !(adjMask & (adjMask - 1) || twoBitsCleared & (twoBitsCleared - 1)) || !twoBitsCleared && oneApartMask && !(oneApartMask & (oneApartMask - 1))
+		// 
+		// Now, notice that twoBitsCleared appears at the start of one one side of the ||, while
+		// !twoBitsCleared appears at the start of the other side. This means we can check
+		// twoBitsCleared first, and only need to compute oneApartMask if it's false
+		// 
+		// So we have:
+		if (twoBitsCleared) {
+			if (adjMask && !(adjMask & (adjMask - 1) || twoBitsCleared & (twoBitsCleared - 1))) {
+				chunkContents |= 1;
+			}
+		} else {
+			uint64_t oneApartMask = clearedBit1 & (clearedBit2 >> 2);
+			
+			if (oneApartMask && !(oneApartMask & (oneApartMask - 1))) {
+				chunkContents |= 1;
+			}
+		}
+		
+	}
+	chunkAggregate* |= chunkContents;
+}
+
+void findAndPrintZeros(uint64_t startSize) {
+	if (startSize < 1) {
+		throw out_of_range("startSize < 1");
+	}
+	
+	vector<struct expansionRegisterColumn> expReg;
+	
+	initialiseExpRegFirstChunk(&expReg, startSize);
+	
+	for (uint64_t curValue = 0; true; curValue += CHUNK_BITS) {
+		
+		uint64_t chunkAggregate = 0x9249249249249249 >> (curValue % 3);
+		// ^ Hex constant is 1001001...1001001
+		// The shift moves it so that the ON bits are the bits which, in the
+		// current chunk, correspond to values that are multiples of 3.
+		
+		//i == 0:
+		if (curValue == 0) {
+			chunkAggregate |= 0b00000000'00000000'00000000'00000001'00000000'00000001'00000001'00010110;
+		} else if (isPowerOf2(curValue / CHUNK_BITS)) {
+			chunkAggregate |= 1;
+		}
+		
+		//i == 1:
+		populateColOneChunk(curValue, &chunkAggregate);
+		
+		// TODO: Currently I don't think the values in col 1 are used in any way to affect col 2,
+		// they just contribute to the aggregate. For col 0 this is fine, but not col 1 (unless we compute col 2 of course).
+		
+		for (int i = 2; i < expReg.size(); i++) {
+			#define currentCol (&(expReg[i])) //can't store a pointer as it'll be invalidated if the vector grows (in copyAlongByPowerOfThree)
+			if (columnHasStarted(currentCol, i, curValue)) {
+				uint64_t chunkContents = getCurrentChunkContents(currentCol);
+				
+				if (chunkContents != 0) { // otherwise no need to do any copying
+					chunkAggregate |= chunkContents;
+					
+					copyAlongByPowerOfThree(&expReg, i, chunkContents, startSize);
+					copyAlongToDoubleCurrentPos(currentCol, chunkContents);
+				}
+				
+				stepForwardChunk(currentCol);
+			}
+			#undef currentCol
+		}
+		
+		if (chunkAggregate != ~0) { // if there are any bits that haven't been set to one - i.e. a value that wasn't reachable
+			// then find those bits & print the values they correspond to
+			for (int i = 0; i < CHUNK_BITS; i++) {
+				if ((~chunkAggregate) & (1ULL << i)) {
+					time_t time_now = chrono::system_clock::to_time_t(chrono::system_clock::now());
+					
+					cout << "\r" << "found zero: " << (curValue + i) << " @ " << ctime(&time_now); // ctime() adds a newline
+				}
+			}
+		}
+		else if (curValue % 5000 == 0) { // print progress every so often. Don't print too often or flush as either may slow things
+			cout << "\r" << "at: " << curValue;
+		}
+	}
+}
+
+void printExpansionRegister(uint64_t startSize) {
+	if (startSize < 1) {
+		throw out_of_range("startSize < 1");
+	}
+	
+	vector<struct expansionRegisterColumn> expReg;
+	
+	initialiseExpRegFirstChunk(&expReg, startSize);
+	
+	for (uint64_t curValue = 0; true; curValue += CHUNK_BITS) {
+		
+		vector<uint64_t> expansionRegPrint;
+		
+		for (int i = 0; i < expReg.size(); i++) {
+			#define currentCol (&(expReg[i])) //can't store a pointer as it'll be invalidated if the vector grows (in copyAlongByPowerOfThree)
+			if (columnHasStarted(currentCol, i, curValue)) {
+				uint64_t chunkContents = getCurrentChunkContents(currentCol);
+				
+				if (chunkContents != 0) { // otherwise no need to do any copying
+					copyAlongByPowerOfThree(&expReg, i, chunkContents, startSize);
+					copyAlongToDoubleCurrentPos(currentCol, chunkContents);
+				}
+				
+				expansionRegPrint.push_back(getCurrentChunkContents(currentCol));
+				
+				stepForwardChunk(currentCol);
+			} else {
+				expansionRegPrint.push_back(0);
+			}
+			#undef currentCol
+		}
+		
+		for (int i = 0; i < CHUNK_BITS; i++) {
+			for (int j = expansionRegPrint.size() - 1; j >= 0; j--) {
+				uint64_t chunk = expansionRegPrint[j];
+				uint64_t bit = chunk & (1ULL << i);
+				cout << (bit != 0 ? "1" : "0");
+			}
+			cout << endl;
+		}
+	}
+}
+
+void printExpansionRegColumn(uint64_t startSize, int printColumn) {
+	if (startSize < 1) {
+		throw out_of_range("startSize < 1");
+	}
+	
+	vector<struct expansionRegisterColumn> expReg;
+	
+	initialiseExpRegFirstChunk(&expReg, startSize);
+	
+	for (uint64_t curValue = 0; true; curValue += CHUNK_BITS) {
+		for (int i = 0; i < expReg.size(); i++) {
+			#define currentCol (&(expReg[i])) //can't store a pointer as it'll be invalidated if the vector grows (in copyAlongByPowerOfThree)
+			if (columnHasStarted(currentCol, i, curValue)) {
+				uint64_t chunkContents = getCurrentChunkContents(currentCol);
+				
+				if (chunkContents != 0) { // otherwise no need to do any copying
+					copyAlongByPowerOfThree(&expReg, i, chunkContents, startSize);
+					copyAlongToDoubleCurrentPos(currentCol, chunkContents);
+				}
+				
+				if (i == printColumn) {
+					printUInt64Bits_cpu_reverse(getCurrentChunkContents(currentCol));
+					//cout << " @ " << (curValue / CHUNK_BITS) << endl;
+					cout << endl;
+				}
+				
+				stepForwardChunk(&expReg[i]);
+			}
+			#undef currentCol
+		}
+	}
+}
+
+int main(int argc, char *argv[]) {
+	
+	if (sizeof(uint64_t) != 8) {
+		cout << "Error: unexpected uint64_t size '" << sizeof(uint64_t) << "', must be 8 bytes" << endl;
+		return -1;
+	}
+	if (sizeof(unsigned int) != 4) {
+		cout << "Error: unexpected unsigned int size '" << sizeof(unsigned int) << "', must be 4 bytes" << endl;
+		return -1;
+	}
+	
+	if (argc < 2) return -1;
+	
+	uint64_t startSize = strtoull(argv[1], nullptr, 10);
+	
+	auto start = chrono::system_clock::now();
+	time_t start_time = chrono::system_clock::to_time_t(start);
+	cout << "Started at: " << ctime(&start_time); // ctime() adds a newline
+	cout << endl;
+	
+	findAndPrintZeros(startSize);
+	//printExpansionRegister(startSize);
+	//printExpansionRegColumn(startSize, 6);
+}
